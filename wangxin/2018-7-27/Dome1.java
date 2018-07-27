5c300a95f6361796691b61a1bd6b80d91d745af4
王鑫 <2417297008@qq.com>
2018-07-27 11:04:13 +0800

    修改修改

--- wangxin/2018-7-27/Dome1.java
+++ wangxin/2018-7-27/Dome1.java
@@ -3,17 +3,15 @@ package com.demo;
 public class Dome1 {
 
 	public static void main(String[] args) {
-		int s=0; int sum=0;
+		 int sum=0; 
 		for(int i=1;i<=20;i++){
-			for(int j=1;j<i;j++){
-				sum=i*j;
-				s=s+sum;
-	
+			int s=1;
+			for(int j=1;j<=i;j++){
+			     s*=j;//s=s*j;	
 			}
-			
+			sum=sum+s;
 		}
-		System.out.println("20的阶乘的值是"+s);
-		
+		System.out.println("20的阶乘的值是"+sum);
 	}
 
 }
